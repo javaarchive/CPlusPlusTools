832f8ef7f4a673b11615337ee6a8fe3ad85f7eb3
Raymond <20248577+javaarchive@users.noreply.github.com>
2019-03-04 19:22:12 -0800

    Add new method to Point

--- CPlusPlusTools/Point.cpp
+++ CPlusPlusTools/Point.cpp
@@ -5,4 +5,5 @@
 
 Point::~Point()
 {
+	
 }
--- CPlusPlusTools/Point.h
+++ CPlusPlusTools/Point.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <iostream>
+#include <cmath>
 class Point
 {
 public:
 	int x;
 	int y;
+	static double getDist(Point x,Point y) {
+		int a = x.x - y.x;
+		int b = x.y - y.y;
+		return std::sqrt(a*a + b*b);
+	}
 	Point() {
 		this->x = 0;
 		this->y = 0;
